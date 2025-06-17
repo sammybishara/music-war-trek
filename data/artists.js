6967200251b9a9171965f560c617039192486180
Chris Minnick <chris@minnick.com>
2025-06-17 15:48:39 -0400

    added data file

---
+++ data/artists.js
@@ -0,0 +1,12 @@
+const artists = [
+  {
+    artistName: '2 Chainz',
+    artistDescription: "He's on a boat!",
+    artistStrength: '10',
+  },
+  {
+    artistName: 'Vanilla Ice',
+    artistDescription: 'Too cold',
+    artistStrength: '1',
+  },
+];
