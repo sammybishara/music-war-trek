7a40696a17a1a45c036ccf79518ce0b86a190197
Chris Minnick <chris@minnick.com>
2025-06-17 16:32:48 -0400

    made artist pages dynamic

--- scripts/artistDetails.js
+++ scripts/artistDetails.js
@@ -1,6 +1,17 @@
-document.getElementById('artistName').innerHTML = artistInfo.artistName;
-document.getElementsByClassName('nav-link')[0] = 'What is this?';
-document.querySelector('#artistDescription').innerHTML =
-  artistInfo.artistDescription;
-document.querySelectorAll('.nav-link')[0].style.color = 'yellow';
-document.getElementsByTagName('p')[0] = 'Welcome!';
+import { artists } from '/data/artists.js';
+
+const urlParams = new URLSearchParams(window.location.search);
+const artistId = urlParams.get('id');
+const thisArtist = artists.find((artist) => (artist.id = artistId));
+
+document.getElementById('artistName').innerHTML = thisArtist.artistName;
+document.getElementById('artistDescription').innerHTML =
+  thisArtist.artistDescription;
+document.getElementById('artistStrength').innerHTML = thisArtist.artistStrength;
+document.getElementById('artistCharisma').innerHTML = thisArtist.artistCharisma;
+document.getElementById('artistAlignment').innerHTML =
+  thisArtist.artistAlignment;
+document.getElementById('artistIntelligence').innerHTML =
+  thisArtist.artistIntelligence;
+document.getElementById('artistSpecialPowers').innerHTML =
+  thisArtist.artistSpecialPowers;
